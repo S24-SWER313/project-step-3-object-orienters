d56a036f4a2045362752d4d6e898967d39f0cd3e
Angela Salem <angel.salem.008@gmail.com>
2024-06-03 23:24:43 +0300

    added client service and cofig-server

---
+++ config-server/src/test/java/object_orienters/techspot/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package object_orienters.techspot.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
