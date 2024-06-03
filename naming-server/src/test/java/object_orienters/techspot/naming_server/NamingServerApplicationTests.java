9c3df8c6fa397dbe0178a42f42955428e8e58df2
Angela Salem <angel.salem.008@gmail.com>
2024-06-04 00:14:01 +0300

    added naming server

---
+++ naming-server/src/test/java/object_orienters/techspot/naming_server/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package object_orienters.techspot.naming_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
