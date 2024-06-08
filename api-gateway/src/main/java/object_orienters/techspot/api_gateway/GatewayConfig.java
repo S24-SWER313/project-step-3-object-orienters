9728bec27eba472e5882ee7c3e487d36ec76dd12
Angela Salem <angel.salem.008@gmail.com>
2024-06-08 21:49:44 +0300

    working on security

--- api-gateway/src/main/java/object_orienters/techspot/api_gateway/GatewayConfig.java
+++ api-gateway/src/main/java/object_orienters/techspot/api_gateway/GatewayConfig.java
@@ -52,16 +52,4 @@ public class GatewayConfig {
 
 }
 
-// @ConfigurationProperties
-// class UriConfiguration {
 
-// private String httpbin = "http://httpbin.org:80";
-
-// public String getHttpbin() {
-// return httpbin;
-// }
-
-// public void setHttpbin(String httpbin) {
-// this.httpbin = httpbin;
-// }
-// }
