81bdcb1984642a9aec7233649b5ae22ebf8f9c38
YousefBandak <145045848+YousefBandak@users.noreply.github.com>
2024-06-08 17:00:14 +0300

    added routes to all content service controllers

--- api-gateway/src/main/java/object_orienters/techspot/api_gateway/GatewayConfig.java
+++ api-gateway/src/main/java/object_orienters/techspot/api_gateway/GatewayConfig.java
@@ -17,101 +17,38 @@ import reactor.core.publisher.Mono;
 @RestController
 public class GatewayConfig {
 
-    @Bean
-    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route("profilePost", p -> p
-                        .path("/profiles")
-                        .and().method(HttpMethod.POST)
-                        // .filters(f -> f.rewritePath("/profiles/(?<username>.*)", "/profiles/${username}"))
-                        .uri("http://localhost:8080"))
+        @Bean
+        public RouteLocator myRoutes(RouteLocatorBuilder builder) {
 
-                // GET request for single profile
-                .route("profileGet", p -> p
-                        .path("/profiles/{username}")
-                        .and().method(HttpMethod.GET)
-                        .filters(f -> f.rewritePath("/profiles/(?<username>.*)", "/profiles/${username}"))
-                        .uri("http://localhost:8080"))
+                return builder
+                                .routes().route(p -> p
+                                                .path("/profiles/**")
+                                                .filters(f -> f.rewritePath("/profiles/(?<segment>.*)",
+                                                                "/profiles/${segment}"))
+                                                .uri("http://localhost:8080"))
+                                .route(p -> p
+                                                .path("/content/**")
+                                                .filters(f -> f.rewritePath("/content/(?<segment>.*)",
+                                                                "/content/${segment}"))
+                                                .uri("http://localhost:8080"))
+                                .route(p -> p
+                                                .path("/tags/**")
+                                                .filters(f -> f.rewritePath("/tags/(?<segment>.*)",
+                                                                "/tags/${segment}"))
+                                                .uri("http://localhost:8080"))
+                                .route(p -> p
+                                                .path("/feed/**")
+                                                .filters(f -> f.rewritePath("/feed/(?<segment>.*)",
+                                                                "/feed/${segment}"))
+                                                .uri("http://localhost:8080"))
+                                .build();
 
-                // GET request for followers with pagination
-                .route("followersGet", p -> p
-                        .path("/profiles/{username}/followers")
-                        .and().method(HttpMethod.GET)
-                        .filters(f -> f.rewritePath("/profiles/(?<username>.*)/followers", "/${username}/followers"))
-                        .uri("http://localhost:8080"))
+        }
 
-                // GET request for specific follower
-                .route("specificFollowerGet", p -> p
-                        .path("/profiles/{username}/follower")
-                        .and().method(HttpMethod.GET)
-                        .filters(f -> f.rewritePath("/profiles/(?<username>.*)/follower", "/${username}/follower"))
-                        .uri("http://localhost:8080"))
-
-                // GET request for following list with pagination
-                .route("followingGet", p -> p
-                        .path("/profiles/{username}/following")
-                        .and().method(HttpMethod.GET)
-                        .filters(f -> f.rewritePath("/profiles/(?<username>.*)/following", "/${username}/following"))
-                        .uri("http://localhost:8080"))
-
-                // GET request for specific following profile
-                .route("specificFollowingGet", p -> p
-                        .path("/profiles/{username}/following/{followingUsername}")
-                        .and().method(HttpMethod.GET)
-                        .filters(f -> f.rewritePath("/profiles/(?<username>.*)/following/(?<followingUsername>.*)",
-                                "/${username}/following/${followingUsername}"))
-                        .uri("http://localhost:8080"))
-
-                // PUT request for updating profile
-                .route("profileUpdatePut", p -> p
-                        .path("/profiles/{username}")
-                        .and().method(HttpMethod.PUT)
-                        .filters(f -> f.rewritePath("/profiles/(?<username>.*)", "/profiles/${username}"))
-                        .uri("http://localhost:8080"))
-
-                // POST request for new follower
-                .route("newFollowerPost", p -> p
-                        .path("/profiles/{username}/followers")
-                        .and().method(HttpMethod.POST)
-                        .filters(f -> f.rewritePath("/profiles/(?<username>.*)/followers", "/${username}/followers"))
-                        .uri("http://localhost:8080"))
-
-                // DELETE request for deleting follower
-                .route("followerDelete", p -> p
-                        .path("/profiles/{username}/followers")
-                        .and().method(HttpMethod.DELETE)
-                        .filters(f -> f.rewritePath("/profiles/(?<username>.*)/followers", "/${username}/followers"))
-                        .uri("http://localhost:8080"))
-
-                // DELETE request for deleting following
-                .route("followingDelete", p -> p
-                        .path("/profiles/{username}/following")
-                        .and().method(HttpMethod.DELETE)
-                        .filters(f -> f.rewritePath("/profiles/(?<username>.*)/following", "/${username}/following"))
-                        .uri("http://localhost:8080"))
-
-                // POST request for adding profile picture
-                .route("profilePicPost", p -> p
-                        .path("/profiles/{username}/profilePic")
-                        .and().method(HttpMethod.POST)
-                        .filters(f -> f.rewritePath("/profiles/(?<username>.*)/profilePic", "/${username}/profilePic"))
-                        .uri("http://localhost:8080"))
-
-                // POST request for adding background image
-                .route("backgroundImgPost", p -> p
-                        .path("/profiles/{username}/backgroundImg")
-                        .and().method(HttpMethod.POST)
-                        .filters(f -> f.rewritePath("/profiles/(?<username>.*)/backgroundImg",
-                                "/${username}/backgroundImg"))
-                        .uri("http://localhost:8080"))
-
-                .build();
-    }
-
-    @RequestMapping("/fallback")
-    public Mono<String> fallback() {
-        return Mono.just("fallback");
-    }
+        @RequestMapping("/fallback")
+        public Mono<String> fallback() {
+                return Mono.just("fallback");
+        }
 
 }
 
