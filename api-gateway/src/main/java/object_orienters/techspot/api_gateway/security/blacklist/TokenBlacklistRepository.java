94b4c921fcbb050a54b9c88fd35c7c3c48232711
YousefBandak <145045848+YousefBandak@users.noreply.github.com>
2024-06-09 23:30:01 +0300

    commit

--- api-gateway/src/main/java/object_orienters/techspot/api_gateway/security/blacklist/TokenBlacklistRepository.java
+++
@@ -1,8 +0,0 @@
-package object_orienters.techspot.api_gateway.security.blacklist;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TokenBlacklistRepository extends JpaRepository<TokenBlackList, String> {
-}
